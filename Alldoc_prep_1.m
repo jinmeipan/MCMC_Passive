5e1ff7169952cf4b17932e52939d40c7d16e11a8
jinmeipan <jinmei.pan@gmail.com>
2016-05-17 21:53:50 -0400

    Add files via upload

---
+++ Alldoc_prep_1.m
@@ -0,0 +1,34 @@
+% All MCMC fortran code input files preparation
+% this is for all R3 results
+clear all
+
+folder0='/Users/office/Downloads/MCMC/MCMCRunData_V2/MCMC_4FREQ_NEW/MCMC_1/';
+mkdir(folder0);
+cd(folder0)
+if(0)
+    open prep_filename
+    open prep_runparam2
+    open prep_tbobs
+    open prep_hyper_gp
+    open prep_hyper_gp_mlyr
+    open prep_hyper_bp
+    open prep_hyper_bp_mlyr
+    open prep_truesp
+end
+
+
+%% Basic settings !!!
+prior_type='gp';  %'gp'=good prior; 'bp'=vic or nldas prior
+stra_type='nostra';  %'stra'=use strategraphy; 'i=nostra'=use homogenous prior
+tb_type='real'; %'real'=real observation; 'syns'=synthetic tb with stdTb;
+
+
+model='hut';   %'emp'=empirical memls; 'iba'='iba','hut'=hut model combination
+stdTb=2.0;
+
+freq_plan='4freq'; %'4freq' or '3freq'.
+
+
+%%
+testyes=0;
+prep_allfiles(folder0,prior_type,stra_type,tb_type,model,stdTb,testyes,freq_plan);
